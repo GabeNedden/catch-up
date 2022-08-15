b86fc8939862f41b42013ce7e71969fb8550c50a
Gabe <gabenedden@gmail.com>
2022-08-15 17:26:01 -0400

    Fix PostContext, start Post function

--- src/contexts/PostContext.js
+++ src/contexts/PostContext.js
@@ -1,30 +1,39 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { UserContext } from "./UserContext";
 export const PostContext = createContext(null);
 
 export const PostProvider = ({ children }) => {
   const { user, isAuthenticated } = useAuth0();
+  const { currentUser } = useContext(UserContext);
   const [postStatus, setPostStatus] = useState("loading");
-  const [posts, setPosts] = useState(null);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetch("https://catch-up-api.herokuapp.com/posts", {
-      method: "GET",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        setPosts(res.data);
-        setPostStatus("loaded");
-      })
-      .catch((error) => {
-        console.log("error:", error);
-        setPostStatus("error");
-      });
-  }, []);
+    if (currentUser) {
+      fetch(
+        `https://catch-up-api.herokuapp.com/sharedpublicposts/${currentUser._id}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          setPosts(res.data);
+          setPostStatus("loaded");
+        })
+        .catch((error) => {
+          console.log("error:", error);
+          setPostStatus("error");
+        });
+    }
+  }, [currentUser]);
+
+  console.log(posts);
 
   return (
     <PostContext.Provider
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -3,10 +3,13 @@ import styled from "styled-components";
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { useParams } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
+import Post from "../components/Post";
 import Avatar from "boring-avatars";
 import GoogleMapReact from "google-map-react";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { PostContext } from "../contexts/PostContext";
 
 const ProfilePage = () => {
   const { userId } = useParams();
@@ -16,6 +19,9 @@ const ProfilePage = () => {
   const [targetUser, setTargetUser] = useState(null);
   const [targetStatus, setTargetStatus] = useState("loading");
   const [reRender, setReRender] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const { postStatus, posts } = useContext(PostContext);
 
   const friendRequest = () => {
     fetch(`https://catch-up-api.herokuapp.com/friendrequest`, {
@@ -118,87 +124,112 @@ const ProfilePage = () => {
 
   return (
     <Wrapper>
-      {targetStatus === "loaded" ? (
-        <Row>
-          <Avatar
-            size={140}
-            name={targetUser.username}
-            variant="beam"
-            colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
-          />
-          <Column>
-            <Display>{targetUser.username}</Display>
-            <Display>Catch Ups: 5</Display>
-            <Display>
-              Friends:{" "}
-              {
-                targetUser.friends.filter(
-                  (friend) => friend.status === "Confirmed"
-                ).length
-              }
-            </Display>
-            {/* user is looking at another users account */}
-            {targetUser?._id !== currentUser?._id &&
-              // some friend status exists
-              (friendStatus ? (
-                friendStatus.status === "Confirmed" ? (
-                  <Button disabled>Friends</Button>
+      <Container>
+        {targetStatus === "loaded" ? (
+          <Row>
+            <Avatar
+              size={140}
+              name={targetUser.username}
+              variant="beam"
+              colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
+            />
+            <Column>
+              <Display>{targetUser.username}</Display>
+              <Display>
+                Catch Ups:{" "}
+                {
+                  posts.filter((post) => {
+                    return post.owner === userId;
+                  }).length
+                }
+              </Display>
+              <Display>
+                Friends:{" "}
+                {
+                  targetUser.friends.filter(
+                    (friend) => friend.status === "Confirmed"
+                  ).length
+                }
+              </Display>
+              {/* user is looking at another users account */}
+              {targetUser?._id !== currentUser?._id &&
+                // some friend status exists
+                (friendStatus ? (
+                  friendStatus.status === "Confirmed" ? (
+                    <Button disabled>Friends</Button>
+                  ) : (
+                    <>
+                      <Button
+                        onClick={() => {
+                          if (friendStatus.initiated) {
+                            updateFriend("Confirmed");
+                          }
+                        }}
+                        disabled={!friendStatus.initiated}
+                      >
+                        {friendStatus.initiated
+                          ? "Accept Request"
+                          : friendStatus.status}
+                      </Button>
+                      <Display>
+                        {friendStatus.initiated
+                          ? "This user sent you a friend request!"
+                          : `Your friend request is ${friendStatus.status}`}
+                      </Display>
+                    </>
+                  )
                 ) : (
-                  <>
-                    <Button
-                      onClick={() => {
-                        if (friendStatus.initiated) {
-                          updateFriend("Confirmed");
-                        }
-                      }}
-                      disabled={!friendStatus.initiated}
-                    >
-                      {friendStatus.initiated
-                        ? "Accept Request"
-                        : friendStatus.status}
-                    </Button>
-                    <Display>
-                      {friendStatus.initiated
-                        ? "This user sent you a friend request!"
-                        : `Your friend request is ${friendStatus.status}`}
-                    </Display>
-                  </>
-                )
-              ) : (
-                // no friend status exists
-                <Button onClick={friendRequest}>Add Friend</Button>
-              ))}
-            {/* user is looking at their own page */}
-            {targetUser?._id === currentUser?._id && (
-              <Display>This is your page!</Display>
-            )}
-          </Column>
-        </Row>
-      ) : null}
+                  // no friend status exists
+                  <Button onClick={friendRequest}>Add Friend</Button>
+                ))}
+              {/* user is looking at their own page */}
+              {targetUser?._id === currentUser?._id && (
+                <Button onClick={() => setOpen(!open)}>
+                  {open ? "Cancel" : "Post a Catch Up!"}
+                </Button>
+              )}
+            </Column>
+          </Row>
+        ) : null}
+      </Container>
 
       {/* start of post/map form */}
-      <div style={{ padding: "1em 0" }} />
-      {userLocation && (
-        <MapWrapper>
-          <GoogleMapReact
-            onClick={(e) => setClickedLocation({ lat: e.lat, lng: e.lng })}
-            defaultZoom={16}
-            defaultCenter={userLocation}
-            bootstrapURLKeys={{
-              key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-            }}
-            yesIWantToUseGoogleMapApiInternals
-          >
-            {clickedLocation && (
-              <StyledIcon
-                lat={clickedLocation.lat}
-                lng={clickedLocation.lng}
-                text="My Marker"
-              />
-            )}
-          </GoogleMapReact>
-        </MapWrapper>
+      {userLocation && open && (
+        <Container>
+          <MapWrapper>
+            <GoogleMapReact
+              onClick={(e) => setClickedLocation({ lat: e.lat, lng: e.lng })}
+              defaultZoom={16}
+              defaultCenter={userLocation}
+              bootstrapURLKeys={{
+                key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+              }}
+              yesIWantToUseGoogleMapApiInternals
+            >
+              {clickedLocation && (
+                <StyledIcon
+                  lat={clickedLocation.lat}
+                  lng={clickedLocation.lng}
+                  text="My Marker"
+                />
+              )}
+            </GoogleMapReact>
+          </MapWrapper>
+        </Container>
       )}
+
+      {postStatus === "loaded" &&
+      posts.filter((post) => {
+        return post.owner === userId;
+      }).length
+        ? posts
+            .filter((post) => {
+              return post.owner === userId;
+            })
+            .map((post) => {
+              return <Post post={post} key={uuidv4()} />;
+            })
+        : "This User has no public posts"}
     </Wrapper>
   );
 };
@@ -209,10 +240,12 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+`;
+
+const Container = styled.div`
   background-color: var(--clr-bg-alt);
   margin: 2em 2em 2em 2em;
   padding: 1em;
-  height: 30em;
   border-radius: 1em;
 `;
 
@@ -269,3 +302,7 @@ const Button = styled.button`
     transform: none;
   }
 `;
+
+const Ul = styled.ul``;
+
+const Li = styled.li``;
